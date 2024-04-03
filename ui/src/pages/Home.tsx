a364c73c113cceabd0880326d891565da5114c35
Paweł <pawelporemba123@gmail.com>
2024-04-03 23:10:54 +0200

    Add header to home page

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,11 +1,24 @@
 import Nav from "../components/Nav";
+import homeStyles from "./Home.module.css";
 
 const Home = () => {
 	return (
-		<div>
+		<>
             <Nav />
-			<h1>Home</h1>
-		</div>
+			<header className={homeStyles.header}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 text-center">
+                            <h1 className="display-3 fw-bold">Witamy na stronie AirTrans</h1>
+                            <p className="lead">Zaplanuj swoją podróż z nami</p>
+                        </div>
+                        <div className="col-6">
+                            Tabela
+                        </div>
+                    </div>
+                </div>
+            </header>
+		</>
 	);
 };
 

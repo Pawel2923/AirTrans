7d96a051b93bcbb12e9fe14dafd3169e68d4d739
Paweł <pawelporemba123@gmail.com>
2024-04-07 18:03:17 +0200

    Add announcements to Home page and modify styles

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -46,57 +46,107 @@ const flightsData: Flight[] = [{
     destination: "FRANKFURT (FRA)"
 }];
 
+interface Announcements {
+    id: number;
+    title: string;
+    content: string;
+    validUntil: Date;
+    personnelId: number;
+}
+
+const announcementsData: Announcements[] = [{
+    id: 1,
+    title: "Zmiana w harmonogramie",
+    content: "Z powodu remontu pasa startowego zmieniono godziny odlotów i przylotów",
+    validUntil: new Date("2021-08-05"),
+    personnelId: 1
+}, {
+    id: 2,
+    title: "Zmiana w harmonogramie",
+    content: "Z powodu remontu pasa startowego zmieniono godziny odlotów i przylotów",
+    validUntil: new Date("2021-08-05"),
+    personnelId: 1
+}, {
+    id: 3,
+    title: "Zmiana w harmonogramie",
+    content: "Z powodu remontu pasa startowego zmieniono godziny odlotów i przylotów",
+    validUntil: new Date("2021-08-05"),
+    personnelId: 1
+
+}];
+
 const Home = () => {    
 	return (
 		<>
             <Nav />
 			<header className={homeStyles.header}>
                 <div className="container">
-                    <div className={`row d-flex align-items-center ${homeStyles["content-wrapper"]}`}>
-                        <div className="col-6 text-center">
+                    <div className={`row d-flex align-items-center ${homeStyles["content-wrapper"]} ${homeStyles["no-padding"]}`}>
+                        <div className="col-lg-6 text-center">
                             <h1 className="display-3 fw-bold">Witamy na stronie AirTrans</h1>
                             <p className="lead">Zaplanuj swoją podróż z nami</p>
                         </div>
-                        <div className="col-6">
+                        <div className="col-lg-6">
                             <ArrDepTable data={flightsData} />
                         </div>
                     </div>
                 </div>
             </header>
             <main>
-                <div className={`d-flex justify-content-around ${homeStyles["content-wrapper"]}`} style={{ backgroundColor: "#B3D3E8" }}>
-                    <div>
-                        <h2 className="display-6 text-uppercase">
-                            Parking
-                        </h2>
-                        <p>
-                            Sprawdź dostępne miejsca parkingowe
-                        </p>
-                        <button type="button" className="btn btn-primary py-2 px-5">
-                            Sprawdź
-                        </button>
+                <div className={`container-fluid ${homeStyles["content-wrapper"]}`} style={{ backgroundColor: "#B3D3E8" }}>
+                    <div className="container-fluid row text-center text-lg-start justify-content-between gap-5 ms-auto mx-lg-auto">
+                        <div className="col-lg-3">
+                            <h2 className="display-6 text-uppercase">
+                                Parking
+                            </h2>
+                            <p>
+                                Sprawdź dostępne miejsca parkingowe
+                            </p>
+                            <button type="button" className="btn btn-primary py-2 px-5">
+                                Sprawdź
+                            </button>
+                        </div>
+                        <div className="col-lg-3">
+                            <h2 className="display-6 text-uppercase">
+                                Wypożyczalnia
+                            </h2>
+                            <p>
+                                Wypożycz samochód z naszego katalogu
+                            </p>
+                            <button type="button" className="btn btn-primary py-2 px-5">
+                                Sprawdź
+                            </button>
+                        </div>
+                        <div className="col-lg-3 d-grid align-items-stretch">
+                            <h2 className="display-6 text-uppercase">
+                                Harmonogram
+                            </h2>
+                            <p>
+                                Sprawdź dostępne loty
+                            </p>
+                            <button type="button" className="btn btn-primary py-2 px-5 align-self-end mx-auto mx-lg-0" style={{ width: "fit-content", height: "fit-content" }}>
+                                Sprawdź
+                            </button>
+                        </div>
                     </div>
-                    <div>
+                </div>
+                <div className={`container-fluid ${homeStyles["content-wrapper"]}`} style={{ backgroundColor: "#eceff2" }}>
+                    <div className="row">
                         <h2 className="display-6 text-uppercase">
-                            Wypożyczalnia
+                            Ogłoszenia
                         </h2>
-                        <p>
-                            Wypożycz samochód z naszego katalogu
-                        </p>
-                        <button type="button" className="btn btn-primary py-2 px-5">
-                            Sprawdź
-                        </button>
                     </div>
-                    <div>
-                        <h2 className="display-6 text-uppercase">
-                            Harmonogram
-                        </h2>
-                        <p>
-                            Sprawdź dostępne loty
-                        </p>
-                        <button type="button" className="btn btn-primary py-2 px-5">
-                            Sprawdź
-                        </button>
+                    <div className={`container-fluid row justify-content-between gap-5 ms-auto mx-lg-auto ${homeStyles["announcements-wrapper"]}`}>
+                        {announcementsData.map((announcement: Announcements, index: number) => (
+                            <div key={index} className="col-md-3">
+                                <h3>
+                                    {announcement.title}
+                                </h3>
+                                <p>
+                                    {announcement.content}
+                                </p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </main>

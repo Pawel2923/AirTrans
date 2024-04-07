1b86444e6cafe85a88aa5956b1aa3aa6e5a8dadb
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-04-07 19:41:10 +0200

    Ustawienia na stronie ogłoszenia

--- ui/src/pages/Ogloszenia.tsx
+++ ui/src/pages/Ogloszenia.tsx
@@ -9,14 +9,16 @@ import './ogloszenia.css';
 const Ogloszenia = () => {
     return (
         <div className="container">
-            <Nav />
-            <h1>Ogłoszenia</h1>
-            <div className="tabela-container">
+            <Nav/>
+            <div style={{width: '100%', backgroundColor: '#ECEFF2',height: '70%',borderRadius:'10px'}}>
+                <h1 style={{marginTop:'2%'}}>Ogłoszenia</h1>
+                <div className="tabela-container" style={{marginTop:'5%'}}>
                 <Tabela />
                 <Tabela />
                 <Tabela />
-                </div>
-            <Footer />
+            </div>
+            </div>
+            <Footer/>
         </div>   
         
     );

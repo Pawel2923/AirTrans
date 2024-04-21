a7964584341db4e3b6b8c327e5882dfd68a7b6a4
Paweł <pawelporemba123@gmail.com>
2024-04-21 10:58:21 +0200

    Add CRUD routes for announcements

--- api/routes/announcements.js
+++ api/routes/announcements.js
@@ -11,4 +11,40 @@ router.get("/", async function (req, res, next) {
     }
 });
 
+router.get("/:id", async function (req, res, next) {
+    try {
+        const { data, response } = await announcements.getById(req.params.id);
+        res.status(response.statusCode).json({ data, message: response.message });
+    } catch (err) {
+        next(JSON.parse(err.message));
+    }
+});
+
+router.post("/", async function (req, res, next) {
+    try {
+        const response = await announcements.create(req.body);
+        res.status(response.statusCode).json({ message: response.message });
+    } catch (err) {
+        next(JSON.parse(err.message));
+    }
+});
+
+router.put("/:id", async function (req, res, next) {
+    try {
+        const response = await announcements.update(req.params.id, req.body);
+        res.status(response.statusCode).json({ message: response.message });
+    } catch (err) {
+        next(JSON.parse(err.message));
+    }
+});
+
+router.delete("/:id", async function (req, res, next) {
+    try {
+        const response = await announcements.remove(req.params.id);
+        res.status(response.statusCode).json({ message: response.message });
+    } catch (err) {
+        next(JSON.parse(err.message));
+    }
+});
+
 module.exports = router;
--- api/services/announcements.js
+++ api/services/announcements.js
@@ -2,12 +2,50 @@ const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
 
+const announcementProperties = [
+	"title",
+	"content",
+	"valid_until",
+	"personnel_id",
+];
+
+function validateAnnouncement(announcement) {
+	// check if all required properties are present
+	helper.checkObject(announcement, announcementProperties);
+
+	const datetimeRegex =
+		/^(((\d{4})-([01]\d)-(0[1-9]|[12]\d|3[01])) (([01]\d|2[0-3]):([0-5]\d):([0-5]\d)))$/m;
+
+	// check if valid_until is in correct format
+	if (!datetimeRegex.test(announcement.valid_until)) {
+		throw new Error(
+			JSON.stringify({
+				statusMessage: "Valid until property invalid format",
+				statusCode: 400,
+			})
+		);
+	}
+
+	// check if valid_until is greater than current date
+	const currentDate = new Date();
+	const validUntilDate = new Date(announcement.valid_until);
+	if (validUntilDate < currentDate) {
+		throw new Error(
+			JSON.stringify({
+				statusMessage:
+					"Valid until date should be greater than current date",
+				statusCode: 400,
+			})
+		);
+	}
+}
+
 async function getAll(page = 1) {
 	const offset = helper.getOffset(page, config.listPerPage);
-	const rows = await db.query(
-		"SELECT * FROM Announcements LIMIT ?,?",
-		[offset, config.listPerPage]
-	);
+	const rows = await db.query("SELECT * FROM Announcements LIMIT ?,?", [
+		offset,
+		config.listPerPage,
+	]);
 	const data = helper.emptyOrRows(rows);
 
 	const pages = await helper.getPages("Announcements", config.listPerPage);
@@ -24,4 +62,83 @@ async function getAll(page = 1) {
 	};
 }
 
-module.exports = { getAll };
+async function getById(id) {
+	const rows = await db.query("SELECT * FROM Announcements WHERE id=?", [id]);
+	const data = helper.emptyOrRows(rows);
+
+	return {
+		data,
+		response: { message: `Successfully fetched data`, statusCode: 200 },
+	};
+}
+
+async function create(announcement) {
+	validateAnnouncement(announcement);
+
+	const result = await db.query("INSERT INTO Announcements SET=?", [
+		announcement,
+	]);
+
+	if (result.affectedRows) {
+		return {
+			message: "Successfully created announcement",
+			statusCode: 201,
+		};
+	} else {
+		throw new Error(
+			JSON.stringify({
+				message: "Failed to create announcement",
+				statusCode: 500,
+			})
+		);
+	}
+}
+
+async function update(id, announcement) {
+	validateAnnouncement(announcement);
+
+	const result = await db.query("UPDATE Announcements SET ? WHERE id=?", [
+		announcement,
+		id,
+	]);
+
+	if (result.affectedRows) {
+		return {
+			message: "Successfully updated announcement",
+			statusCode: 200,
+		};
+	} else {
+		throw new Error(
+			JSON.stringify({
+				message: "Failed to update announcement",
+				statusCode: 500,
+			})
+		);
+	}
+}
+
+async function remove(id) {
+	const result = await db.query("DELETE FROM Announcements WHERE id=?", [id]);
+
+	if (result.affectedRows) {
+		return {
+			message: "Successfully deleted announcement",
+			statusCode: 200,
+		};
+	} else {
+		throw new Error(
+			JSON.stringify({
+				message: "Failed to delete announcement",
+				statusCode: 500,
+			})
+		);
+	}
+}
+
+module.exports = {
+	getAll,
+	getById,
+	create,
+	update,
+	remove,
+};

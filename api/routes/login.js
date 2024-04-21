5c74dd8c28cc293b978c3867419ba52505292b27
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-04-21 18:19:10 +0200

    do poprawy

--- api/routes/login.js
+++ api/routes/login.js
@@ -8,37 +8,25 @@ router.post("/", async function (req, res, next) {
     const { email, password } = req.body;
 
     try {
-        const { response, id, storedHashedPassword, userInputPassword } = await loginService.fetchClient(email, password);
+        const { response, id } = await loginService.fetchClient(email, password);
         
-        if (response.statusCode === 404) {
-            console.log("Użytkownik nie znaleziony");
+        if (response.statusCode !== 200) {
+            console.log(response.message);
             return res.status(response.statusCode).json({ message: response.message });
         }
 
-        bcrypt.compare(userInputPassword, storedHashedPassword, (err, result) => {
-            if (err) {
-                console.error("Błąd porównywania haseł:", err);
-                throw new Error({
-                    message: "Błąd porównywania haseł",
-                    statusCode: 500,
-                });
-            }
-    
-            if (result) {
-                const token = jwt.sign({ id }, process.env.SECRET_TOKEN, {
-                    expiresIn: 86400,
-                });
-                const refreshToken = jwt.sign({ id }, process.env.REFRESH_SECRET_TOKEN, {
-                    expiresIn: 525600,
-                });
-             
-                res.setHeader("Authorization", `Bearer ${token}`,`refresh ${refreshToken}` );
-                return res.status(200).json({ auth: true });
-            } else {
-                console.log("Nieprawidłowe hasło");
-                return res.status(401).json({ auth: false });
-            }
+        const token = jwt.sign({ id }, process.env.SECRET_TOKEN, {
+            expiresIn: 86400,
         });
+        const refreshToken = jwt.sign({ id }, process.env.REFRESH_SECRET_TOKEN, {
+            expiresIn: 525600,
+        });
+     
+        res.set({
+            "Authorization": `Bearer ${token}`,
+            "Refresh-Token": `Bearer ${refreshToken}`
+        });
+        return res.status(200).json({ auth: true });
     } catch (err) {
         next(err);
     }
--- api/services/login.js
+++ api/services/login.js
@@ -2,37 +2,48 @@ const db = require("./db");
 const bcrypt = require("bcryptjs");
 
 async function fetchClient(email, userInputPassword) {
-	if (!email || !userInputPassword) {
-		return {
-			response: { message: "Invalid arguments: email and userInputPassword are required", statusCode: 400 },
-		};
-	}
+    if (!email || !userInputPassword) {
+        return {
+            response: { message: "Invalid arguments: email and userInputPassword are required", statusCode: 400 },
+        };
+    }
 
-	const queryCheckUser = "SELECT IF(COUNT(*) = 0, 0, 1) AS user_exists  FROM Client WHERE Email = ?";
-	const rows = await db.query(queryCheckUser, [email]);
+    try {
+        //tu pobranie jest soli 
+        const queryGetSalt = "SELECT Salt FROM Client WHERE Email = ?";
+        const saltResult = await db.query(queryGetSalt, [email]);
 
-	if (rows.length === 0 || rows[0].user_exists === 0) {
-		return {
-			response: { message: "User not found", statusCode: 404 },
-		};
-	}
+        if (saltResult.length === 0) {
+            return {
+                response: { message: "User not found", statusCode: 404 },
+            };
+        }
 
-	const storedHashedPassword = rows[0].Password;
+        const salt = saltResult[0].Salt;
 
-	// Compare the user input password with the hashed password stored in the database
-	const passwordMatch = await bcrypt.compare(userInputPassword, storedHashedPassword);
+        //tu haszujes haslo od uzytkowniak 
+        const hashedPassword = await bcrypt.hash(userInputPassword, salt);
 
-	if (!passwordMatch) {
-		return {
-			response: { message: "Invalid password", statusCode: 401 },
-		};
-	}
+        //tu sprawdza czy pasuje 
+        const queryCheckUser = "SELECT COUNT(*) AS user_exists FROM Client WHERE Email = ? AND Password = ?";
+        const userExistsRows = await db.query(queryCheckUser, [email, hashedPassword]);
+//w tym ifie mi sie zatrzymuje jak wprowadze poprawne dane i nwm czemu 
+        if (userExistsRows.length === 0 || userExistsRows[0].user_exists === 0) {
+            return {
+                response: { message: "Incorrect email or password", statusCode: 401 },
+            };
+        }
 
-	// Password is correct, so return a response with status code 200
-	return { response: { statusCode: 200 }, id: email };
+        
+        return { response: { statusCode: 200 }, id: email };
+    } catch (error) {
+        console.error("Error fetching client:", error);
+        return {
+            response: { message: "Internal server error", statusCode: 500 },
+        };
+    }
 }
 
 module.exports = {
-	fetchClient,
+    fetchClient,
 };
-

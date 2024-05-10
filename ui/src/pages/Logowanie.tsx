fa38743040d90e5489a94764feb23ad4ea4a0ac2
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-05-10 15:08:28 +0200

    logowanie styl

--- ui/src/pages/Logowanie.tsx
+++ ui/src/pages/Logowanie.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react"; // Zmiana importu na pełną ścieżkę do pliku "react"
+import React, { useState } from "react";
 import loginService from "../services/login.service";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/input";
-
-
+import styles from "./Logowanie.module.css"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowLeftLong,
+} from "@fortawesome/free-solid-svg-icons";
 const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i;
 const isEmpty = (value: string) => value.trim() !== "" && value.trim().length >= 3;
 const isEmail = (value: string) =>
   emailRegex.test(value.toLowerCase()) &&
   value.toLowerCase().trim().length >= 3;
-
-
 
 const Login = () => {
   const navigate = useNavigate();
@@ -39,7 +39,7 @@ const Login = () => {
         alert("Formularz jest niepoprawny");
         return;
       }
-  
+
       const response = await loginService.create({ email, password });
       if (response.status === 200) {
         const { auth } = response.data;
@@ -57,58 +57,64 @@ const Login = () => {
       alert("Błąd logowania - sprawdź konsolę");
     }
   };
-  const loginForm = (
-    <form onSubmit={submitHandler}>
-      <label>
-        <p>E-mail*</p>
-        <Input
-          type="email"
-          id="email"
-          value={email}
-          minLength={3}
-          onInput={emailInputHandler}
-          validateInput={isEmail}
-          setIsFormInvalid={setIsFormInvalid}
-          required
-        />
-        
-      </label>
-      <label>
-        <p>Hasło*</p>
-        <Input
-          type="password"
-          id="password"
-          value={password}
-          minLength={3}
-          onInput={passwordInputHandler}
-          validateInput={isEmpty}
-          setIsFormInvalid={setIsFormInvalid}
-          required
-        />
-
-      </label>
-      <button type="submit">Zaloguj się</button>
-      <p>
-        Nie masz jeszcze konta? <Link to="/rejestracja">Zapisz się</Link>
-      </p>
-    </form>
-
-  );
 
   return (
-    <>
-      <main>
-        <section className="section">
-          <h1>Zaloguj się</h1>
-          <h2>Wypełnij formularz logowania</h2>
-          {loginForm}
-        </section>
-      </main>
-    </>
-    );
-  };
+    <main>
+      <section className="section">
+        <div className={`container ${styles.container}`}>
+          <div className={`rounded p-4 bg-white ${styles['login-container']}`}>
+            <div className={styles.left}>
+              <h1 className="text-center mb-4">LOGOWANIE DO SYSTEMU</h1>
+              <p className="text-center">
+                Nie masz jeszcze konta? <Link to="/rejestracja">Zapisz się</Link>
+              </p>
+              <p className="text-center">
+                Zresetuj hasło <Link to="/resetowanie">tutaj</Link>.
+              </p>
+              <p >
+              <Link to="/" className="link" style={{ color: 'black' }}>
+                <FontAwesomeIcon icon={faArrowLeftLong} /> Wróć
+                </Link>
+              </p>
+            </div>
+            <div className={styles.right}>
+              <form onSubmit={submitHandler}>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">E-mail*</label>
+                  <Input
+                    type="email"
+                    id="email"
+                    value={email}
+                    minLength={3}
+                    onInput={emailInputHandler}
+                    validateInput={isEmail}
+                    setIsFormInvalid={setIsFormInvalid}
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Hasło*</label>
+                  <Input
+                    type="password"
+                    id="password"
+                    value={password}
+                    minLength={3}
+                    onInput={passwordInputHandler}
+                    validateInput={isEmpty}
+                    setIsFormInvalid={setIsFormInvalid}
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary" style={{ backgroundColor: "#5DA9DB" }}>Zaloguj się</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-
-
-
-  export default Login;
+export default Login;

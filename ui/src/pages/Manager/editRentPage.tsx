d53986fa6db205d85745ead27cc03412433f4017
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-05-19 20:41:57 +0200

    poprawki w stylu edycji

--- ui/src/pages/Manager/editRentPage.tsx
+++ ui/src/pages/Manager/editRentPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import rentalService from "../../services/rental.service";
 import { Rentals } from "../../assets/Data";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const emptyRental: Rentals = {
   id: 0,
@@ -9,116 +10,134 @@ const emptyRental: Rentals = {
   until: "",
   Users_uid: 0,
   Cars_id: 0,
-  status:undefined,
+  status: undefined,
 };
 
 const EditRentPage = () => {
-    const navigate = useNavigate();
-    const { id } = useParams<{ id: string }>();
-    const [carRental, setCarRent] = useState<Rentals>(emptyRental);
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const [carRental, setCarRent] = useState<Rentals>(emptyRental);
 
-    useEffect(() => {
-        if (id === undefined) return;
+  useEffect(() => {
+    if (id === undefined) return;
 
-        const rentId = parseInt(id);
+    const rentId = parseInt(id);
 
-        rentalService.getById(rentId).then((response) => {
-            if (response.status === 200) {
-                setCarRent(response.data.data[0]);
-            }
-        });
-    }, [id]);
+    rentalService.getById(rentId).then((response) => {
+      if (response.status === 200) {
+        setCarRent(response.data.data[0]);
+      }
+    });
+  }, [id]);
 
-    const formSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const formSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        try {
-            const response = await rentalService.updateRent(carRental);
-            if (response.status === 200) {
-                alert("Edycja zakończona sukcesem!");
-                navigate("/zarzadzanie/pojazd");
-            }
-        } catch (error) {
-            console.error("Error while updating rent:", error);
-            alert("Wystąpił błąd podczas aktualizacji wypożyczenia. Spróbuj ponownie");
-        }
-    };
+    try {
+      const response = await rentalService.updateRent(carRental);
+      if (response.status === 200) {
+        alert("Edycja zakończona sukcesem!");
+        navigate("/zarzadzanie/pojazd");
+      }
+    } catch (error) {
+      console.error("Error while updating rent:", error);
+      alert("Wystąpił błąd podczas aktualizacji wypożyczenia. Spróbuj ponownie");
+    }
+  };
 
-    const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCarRent({
-            ...carRental,
-            [e.target.name]: e.target.value,
-        });
-    };
-    const selectChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setCarRent({
-            ...carRental,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCarRent({
+      ...carRental,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div>
-            <h1>Edit Rent</h1>
-            <form onSubmit={formSubmitHandler}>
-                <label>
-                    Rental Date:
-                    <input
-                        type="datetime-local"
-                        name="since"
-                        placeholder="Rental Date"
-                        value={carRental.since?.toString() }
-                        onChange={inputChangeHandler}
-                    />
-                </label>
-                <label>
-                    Return Date:
-                    <input
-                        type="datetime-local"
-                        name="until"
-                        placeholder="Return Date"
-                        value={carRental.until?.toString()}
-                        onChange={inputChangeHandler}
-                    />
-                </label>
-                <label>
-                    status:
-                    <select
-                        name="status"
-                        value={carRental.status}
-                        className="form-control"
-                        onChange={selectChangeHandler}
-                    >
-                        <option value="PENDING">Zarezerwowane</option>
-                        <option value="RENTED">Wypożyczone</option>
-                        <option value="RETURNED">Zwrócone</option>
-                        <option value="CANCELLED">Anulowane</option>
-                    </select>
-                </label>
-                <label>
-                    Client ID:
-                    <input
-                        type="number"
-                        name="Users_uid"
-                        placeholder="Client ID"
-                        value={carRental.Users_uid}
-                        onChange={inputChangeHandler}
-                    />
-                </label>
-                <label>
-                    Car ID:
-                    <input
-                        type="number"
-                        name="Cars_id"
-                        placeholder="Car ID"
-                        value={carRental.Cars_id}
-                        onChange={inputChangeHandler}
-                    />
-                </label>
-                <button type="submit">Zapisz zmiany</button>
-            </form>
-        </div>
-    );
+  const selectChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCarRent({
+      ...carRental,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <Container>
+      <h1 className="text-center">Edytuj Wypożyczenie</h1>
+      <Form onSubmit={formSubmitHandler}>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Data wypożyczenia</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                name="since"
+                placeholder="Data wypożyczenia"
+                value={carRental.since?.toString()}
+                onChange={inputChangeHandler}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Data zwrotu</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                name="until"
+                placeholder="Data zwrotu"
+                value={carRental.until?.toString()}
+                onChange={inputChangeHandler}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Status</Form.Label>
+              <Form.Select
+                name="status"
+                value={carRental.status}
+                onChange={selectChangeHandler}
+              >
+                <option value="PENDING">Zarezerwowane</option>
+                <option value="RENTED">Wypożyczone</option>
+                <option value="RETURNED">Zwrócone</option>
+                <option value="CANCELLED">Anulowane</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>ID Klienta</Form.Label>
+              <Form.Control
+                type="number"
+                name="Users_uid"
+                placeholder="ID Klienta"
+                value={carRental.Users_uid}
+                onChange={inputChangeHandler}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>ID Samochodu</Form.Label>
+              <Form.Control
+                type="number"
+                name="Cars_id"
+                placeholder="ID Samochodu"
+                value={carRental.Cars_id}
+                onChange={inputChangeHandler}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Button variant="primary" type="submit">
+          Zapisz zmiany
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default EditRentPage;

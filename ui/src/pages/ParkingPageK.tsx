e6d041fcd10ef1b57368ca230ce1fc260de5531a
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2024-05-29 17:25:30 +0200

    strona_rezerwacji_parkingu

--- ui/src/pages/ParkingPageK.tsx
+++ ui/src/pages/ParkingPageK.tsx
@@ -1,24 +1,60 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-//import { ParkingReservations } from '../assets/Data';
+import React, { useState, useEffect } from 'react';
+import styles from './parkingPagek.module.css';
+import { Link } from 'react-router-dom';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
-
-
-const ParkingPageK = () => {
-    //const navigate = useNavigate();
-
-   // const choosePlace = (parking: ParkingReservations) => {
-     //   navigate(`data/${parking.id}`);
-    //}
-
-    return (
-        <div>
-            <h1>Parking Page</h1>
-            
-           
-
-        </div>
-    );
+interface RangeType {
+    selection: {
+        startDate: Date;
+        endDate: Date;
+        key: string;
+    };
 }
 
-export default ParkingPageK;
+const ParkingReservation = () => {
+    const [selectionRange, setSelectionRange] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+    });
+
+    const handleSelect = (ranges: RangeType) => {
+        setSelectionRange(ranges.selection);
+    };
+
+    useEffect(() => {
+        sessionStorage.setItem('reservationDates', JSON.stringify(selectionRange));
+    }, [selectionRange]);
+
+    const daysCount = Math.round((selectionRange.endDate.getTime() - selectionRange.startDate.getTime()) / (1000 * 3600 * 24)) + 1;
+
+    return (
+        <div className={styles.pageContainer}>
+            <header className={styles.header}>
+                <h1>ZAREZERWUJ MIEJSCE PARKINGOWE</h1>
+            </header>
+            <div className="row">
+                <DateRangePicker
+                    ranges={[selectionRange]}
+                    onChange={handleSelect}
+                />
+                <div className={styles.dateInfo}>
+                    {`${selectionRange.startDate.toLocaleDateString()} - ${selectionRange.endDate.toLocaleDateString()}`}
+                </div>
+                <div className={styles.totalPrice}>
+                    Cena rezerwacji za {daysCount} dni: {daysCount * 50} PLN
+                </div>
+            </div>
+            <div className={styles.nextButtonContainer}>
+                <Link to="form" className="btn btn-primary py-2 px-5">
+                    Przejdź dalej
+                    <span>&#10132;</span>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default ParkingReservation;

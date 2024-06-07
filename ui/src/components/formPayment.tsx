77adab5ebe0372c5fc6be206158387a3825f3938
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2024-06-07 15:00:38 +0200

    płatnosc parking

--- ui/src/components/formPayment.tsx
+++ ui/src/components/formPayment.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { CardElement, useStripe, useElements, P24BankElement } from '@stripe/react-stripe-js';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './formPayment.module.css'; 
 import parkingService from '../services/parking.service';
@@ -11,154 +11,112 @@ const CheckoutForm: React.FC = () => {
   const navigate = useNavigate();
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<boolean>(false);
-  const [paymentMethodType, setPaymentMethodType] = useState<'card' | 'p24'>('card');
   const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const location = useLocation();
   const { totalPrice } = location.state as { totalPrice: number };
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     if (!stripe || !elements) {
       return;
     }
 
-    const saveReservation = () => {
-      const reservationDates = JSON.parse(sessionStorage.getItem('reservationDates') || '{}') || {};
-      const sinceDate = new Date(reservationDates.startDate);
-      const untilDate = new Date(reservationDates.endDate);
-      const licensePlate = sessionStorage.getItem('licensePlate') || '';
-      const parkingLevel = sessionStorage.getItem('parkingLevel') || '';
-      const spaceId = sessionStorage.getItem('spaceId') || '';
+    const cardElement = elements.getElement(CardElement);
 
-      const parkingReservation = {
-        pid: 0,
-        parking_level: parkingLevel,
-        space_id: spaceId, 
-        Users_id: 7, 
-        status: 'Reserved',
-        license_plate: licensePlate,
-        since: sinceDate.toISOString().slice(0, 19).replace('T', ' '), 
-        until: untilDate.toISOString().slice(0, 19).replace('T', ' '),
-      };
+    if (!cardElement) {
+      return;
+    }
 
-      if (parkingReservation.parking_level && parkingReservation.license_plate && parkingReservation.since && parkingReservation.until) {
-        parkingService.createParking({...parkingReservation, status: 'RESERVED'})
-          .then((response) => {
-            console.log('Reservation created:', response);
-          })
-          .catch((error) => {
-            console.error('Error creating reservation:', error);
-          });
-      } else {
-        console.error('Missing required fields:', parkingReservation);
+    try {
+      const paymentMethod = await stripe.createPaymentMethod({
+        type: 'card',
+        card: cardElement,
+        billing_details: {
+          name,
+          email,
+        },
+      });
+
+      if (paymentMethod.error) {
+        throw new Error(paymentMethod.error.message || 'Payment failed');
       }
-    };
 
-    if (paymentMethodType === 'card') {
-      const cardElement = elements.getElement(CardElement);
+      const response = await fetch('http://localhost:6868/stripe/create-payment-intent', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: totalPrice * 100 }),
+      });
 
-      if (cardElement) {
-        stripe.createPaymentMethod({
-          type: 'card',
+      const { clientSecret } = await response.json();
+
+      const { error: confirmError, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
           card: cardElement,
           billing_details: {
             name,
             email,
           },
-        })
-        .then(({ error, paymentMethod }) => {
-          if (error) {
-            throw new Error(error.message || 'Payment failed');
-          }
+        },
+      });
 
-          return fetch('http://localhost:6868/stripe/create-payment-intent', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ amount: totalPrice * 100 }),
-          });
-        })
-        .then(response => response.json())
-        .then(({ clientSecret }) => {
-          const cardElement = elements.getElement(CardElement);
-          if (!cardElement) throw new Error('Card element not found');
-
-          return stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-              card: cardElement,
-              billing_details: {
-                name,
-                email,
-              },
-            },
-          });
-        })
-        .then(({ error: confirmError }) => {
-          if (confirmError) {
-            throw new Error(confirmError.message || 'Payment confirmation failed');
-          }
-
-          setError(null);
-          setSuccess(true);
-          saveReservation();
-          navigate('/payment/success');
-        })
-        .catch(error => {
-          setError(error.message || 'An error occurred while processing your payment');
-          setSuccess(false);
-          navigate('/payment/error');
-        });
+      if (confirmError) {
+        throw new Error(confirmError.message || 'Payment confirmation failed');
       }
-    } else if (paymentMethodType === 'p24') {
-      const p24Element = elements.getElement(P24BankElement);
 
-      if (p24Element) {
-        fetch('http://localhost:6868/stripe/create-payment-intent', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ amount: totalPrice * 100 }),
-        })
-        .then(response => response.json())
-        .then(({ clientSecret }) => {
-          return stripe.confirmP24Payment(clientSecret, {
-            payment_method: {
-              p24: p24Element,
-              billing_details: {
-                name,
-                email,
-              },
-            },
-            return_url: `${window.location.origin}/payment/success`,
-          });
-        })
-        .then(({ error: confirmError }) => {
-          if (confirmError) {
-            throw new Error(confirmError.message || 'Payment confirmation failed');
-          }
-
-          setError(null);
-          setSuccess(true);
-          saveReservation();
-          navigate('/payment/success');
-        })
-        .catch(error => {
-          setError(error.message || 'An error occurred while processing your payment');
-          setSuccess(false);
-          navigate('/payment/error');
-        });
+      if (paymentIntent && paymentIntent.status === 'succeeded') {
+        setError(null);
+        setSuccess(true);
+        saveReservation();
+        navigate('/payment/success');
+      } else {
+        throw new Error('Payment failed');
       }
+    } catch (error) {
+      setError( 'An error occurred while processing your payment');
+      setSuccess(false);
+      navigate('/payment/error');
+    }
+  };
+
+  const saveReservation = async () => {
+    const reservationDates = JSON.parse(sessionStorage.getItem('reservationDates') || '{}') || {};
+    const sinceDate = new Date(reservationDates.startDate);
+    const untilDate = new Date(reservationDates.endDate);
+    const licensePlate = sessionStorage.getItem('licensePlate') || '';
+    const parkingLevel = sessionStorage.getItem('parkingLevel') || '';
+    const spaceId = sessionStorage.getItem('spaceId') || '';
+
+    const parkingReservation = {
+      pid: 0,
+      parking_level: parkingLevel,
+      space_id: spaceId, 
+      Users_id: 7, 
+      status: 'Reserved',
+      license_plate: licensePlate,
+      since: sinceDate.toISOString().slice(0, 19).replace('T', ' '), 
+      until: untilDate.toISOString().slice(0, 19).replace('T', ' '),
+    };
+
+    try {
+      if (parkingReservation.parking_level && parkingReservation.license_plate && parkingReservation.since && parkingReservation.until) {
+        const response = await parkingService.createParking({...parkingReservation, status: 'RESERVED'});
+        console.log('Reservation created:', response);
+      } else {
+        console.error('Missing required fields:', parkingReservation);
+      }
+    } catch (error) {
+      console.error('Error creating reservation:', error);
     }
   };
 
   return (
     <div className="container">
       <div className="card mx-auto my-5 p-4" style={{ maxWidth: '500px' }}>
-      <h2 className="mb-4">Zapłać teraz</h2>
+        <h2 className="mb-4">Zapłać teraz</h2>
         <form onSubmit={handleSubmit} className="checkout-form">
           <div className="form-group">
             <label htmlFor="name">Imie</label>
@@ -187,25 +145,17 @@ const CheckoutForm: React.FC = () => {
             <select
               id="payment-method-type"
               className="form-control"
-              value={paymentMethodType}
-              onChange={(e) => setPaymentMethodType(e.target.value as 'card' | 'p24')}
+              value="card"
+              onChange={() => {}}
+              disabled
             >
               <option value="card">Karta</option>
-              <option value="p24">Przelewy24</option>
             </select>
           </div>
-          {paymentMethodType === 'card' && (
-            <div className="form-group">
-              <label htmlFor="card-element">Szczegóły Karty</label>
-              <CardElement id="card-element" className="form-control" />
-            </div>
-          )}
-          {paymentMethodType === 'p24' && (
-            <div className="form-group">
-              <label htmlFor="p24-element">Przelewy24</label>
-              <P24BankElement id="p24-element" className="form-control" />
-            </div>
-          )}
+          <div className="form-group">
+            <label htmlFor="card-element">Szczegóły Karty</label>
+            <CardElement id="card-element" className="form-control" />
+          </div>
           <button type="submit" className="btn btn-primary btn-block mt-4" disabled={!stripe}>
             Pay {totalPrice} PLN
           </button>

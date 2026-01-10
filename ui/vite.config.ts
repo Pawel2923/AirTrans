fdce66ba651b028de48bff3137790f665461a83e
Pawel2923 <pawelporemba123@gmail.com>
2026-01-10 11:49:11 +0100

    fix: dev vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,19 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    hmr: {
-      clientPort: 3000,
-    },
-    port: 80,
-    watch: {
-      usePolling: true,
-    },
-  },
-  plugins: [react()],
-  // build: {
-  //   emptyOutDir: true,
-  //   chunkSizeWarningLimit: 800,
+  // server: {
+  //   host: "0.0.0.0",
+  //   hmr: {
+  //     clientPort: 3000,
+  //   },
+  //   port: 80,
+  //   watch: {
+  //     usePolling: true,
+  //   },
   // },
+  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 800,
+  },
 });
